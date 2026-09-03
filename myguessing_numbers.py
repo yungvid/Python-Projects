f86753ea7fc015d8d6c43d1c9596a9046613a3af
Idowu Victor <101068031+yungvid@users.noreply.github.com>
2026-09-02 20:23:31 -0700

    Fix indentation and comments in guessing game

--- myguessing_numbers.py
+++ myguessing_numbers.py
@@ -11,11 +11,11 @@ while True:
     guess = int(input("Enter a guess: ")) # Convert user input and convert it to a whole number
     attempts += 1
 
-    if guess < hidden_number:
+    if guess < hidden_number: # Checking the guess number if it's less than hidden number
         print("Too low! Try again.")
-    elif guess > hidden_number:
+    elif guess > hidden_number: # Checking the guess number if it's greater than hidden number
         print("Too high! Try again.")
-    else:
+    else: # Print if the guess number == hidden number
         print(f"Congratulations! You guessed the number {hidden_number} in {attempts} attempts.")
         break
 
