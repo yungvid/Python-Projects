0ed503ff9710d83b4a5aaad47914bc128cefe306
Idowu Victor <vidd8963@gmail.com>
2026-09-02 20:16:13 -0700

    Create myguessing_numbers.py

---
+++ myguessing_numbers.py
@@ -0,0 +1,22 @@
+import random
+
+hidden_number = random.randint(1, 100)
+attempts = 0
+
+print("-------Welcome to my Guessing game----------")
+print("I have selected a number between 1 and 100. Can you guess it?")
+
+while True:
+
+    guess = int(input("Enter a guess: ")) # Convert user input and convert it to a whole number
+    attempts += 1
+
+    if guess < hidden_number:
+        print("Too low! Try again.")
+    elif guess > hidden_number:
+        print("Too high! Try again.")
+    else:
+        print(f"Congratulations! You guessed the number {hidden_number} in {attempts} attempts.")
+        break
+
+
